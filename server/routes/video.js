8cd85dddc095b3018421e00c2dcbfb05429aed89
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-06-21 19:15:14 +0900

    # 랜딩페이지에 비디오 썸네일 출력

--- server/routes/video.js
+++ server/routes/video.js
@@ -49,6 +49,16 @@ router.post('/uploadVideo', (req, res) => {
     })
 })
 
+router.get('/getVideos', (req, res) => {
+    //비디오를 DB에서 가져와서 클라이언트에 전송
+    Video.find()
+    .populate('writer') //모든 정보를 가져오기 위해 populate
+    .exec((err, videos) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).json({ success:true, videos })
+    })
+})
+
 router.post('/thumbnail', (req, res) => {
     let filePath = ""
     let fileDuration = ""
